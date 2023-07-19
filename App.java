372c7921d428d00909a79ac188901312756843f2
tush045 <108879417+tush045@users.noreply.github.com>
2023-07-19 14:57:20 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+class Student {
+    String name;
+    int subject1;
+    int subject2;
+    int subject3;
+
+    public Student(String name, int subject1, int subject2, int subject3) {
+        this.name = name;
+        this.subject1 = subject1;
+        this.subject2 = subject2;
+        this.subject3 = subject3;
+    }
+
+    public void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Subject 1: " + subject1);
+        System.out.println("Subject 2: " + subject2);
+        System.out.println("Subject 3: " + subject3);
+    }
+}
+class StudentDataEntry {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Student[] students = new Student[10];
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Enter data for student " + (i + 1) + ":");
+
+            System.out.print("Name: ");
+            String name = scanner.nextLine();
+
+            System.out.print("Subject 1: ");
+            int subject1 = scanner.nextInt();
+
+            System.out.print("Subject 2: ");
+            int subject2 = scanner.nextInt();
+
+            System.out.print("Subject 3: ");
+            int subject3 = scanner.nextInt();
+
+            students[i] = new Student(name, subject1, subject2, subject3);
+            System.out.println();
+        }
+
+        // Display the entered data
+        System.out.println("Entered student data:");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Student " + (i + 1) + ":");
+            students[i].display();
+            System.out.println();
+        }
+    }
+}
